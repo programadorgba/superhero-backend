974521530ce4c9cf902fffb18dd068598d315705
programadorgba <programador@gmail.com>
2026-02-06 03:25:34 +0100

    cambios en el archivo env.js

--- config/env.js
+++ config/env.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const SUPERHERO_API_KEY = process.env.SUPERHERO_API_KEY || '';
 
 module.exports = {
-  PORT: process.env.PORT ? parseInt(process.env.PORT) : 3000,
+  PORT: process.env.PORT || 3000,  // ← QUITA el parseInt, Render ya lo maneja
   SUPERHERO_API_KEY: SUPERHERO_API_KEY,
   COMICVINE_API_KEY: process.env.COMICVINE_API_KEY || '',
   SUPERHERO_BASE_URL: `https://www.superheroapi.com/api.php/${SUPERHERO_API_KEY}`
